23381208a6c2e55ad3a67d6ce18de7766f56eb1d
vojtech duffek <vojta.duffek@gmail.com>
2025-03-27 15:01:07 +0100

    --

--- src/CARDGAME/deck.py
+++ src/CARDGAME/deck.py
@@ -6,13 +6,16 @@ class Deck:
         for color in Card.COLORS:
             for value in range(2, 15):
                 self.cards = Card(value, color)
+   
     def shuffle(self):
         random.shuffle(self.cards)
+   
     def deal_card(self):
         if self.cards:
             return self.cards.pop()
         else:
             None
+   
     def split_deck_for_players(self, num_players):
         for i in range(num_players):
             return self.cards[i::num_players]
--- src/CARDGAME/player.py
+++ src/CARDGAME/player.py
@@ -1,13 +1,18 @@
 class Player:
+
     def __init__(self, cards, name):
         self.cards = cards
         self.name = name
+
     def has_cards(self):
         return len(self.cards) > 0
+
     def play_card(self):
         if self.has_cards > 0:
             return self.cards.pop(0)
+
     def collect_cards(self, won_cards):
         return self.cards.extend(won_cards)
+
     def __str__(self):
         return f"{self.name} has {len(self.cards)} cards"
